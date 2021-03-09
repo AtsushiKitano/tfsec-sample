d8e81eecd51622e2969df5771e50b5891db3a4a6
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-03-09 14:20:36 +0900

    [mod] Inboudルールを無効化するルールコメントの追加

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_compute_firewall" "main" {
   target_tags = []
 
   direction     = "INGRESS"
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["0.0.0.0/0"] #tfsec:ignore:GCP003
 
   allow {
     protocol = "tcp"
